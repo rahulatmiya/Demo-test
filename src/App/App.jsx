2d36389692dcd8ee1ccaad89216d8adb6467f378
rahulatmiya <rahulatmiya@gmail.com>
2024-12-18 01:12:31 +0530

    React basic project

---
+++ src/App/App.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
