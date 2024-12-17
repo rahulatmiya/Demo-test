930f575d6a913b4327ae33d8589b3dca7199e14a
rahulatmiya <rahulatmiya@gmail.com>
2024-12-18 02:03:14 +0530

    working code

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import React from 'react'; // Ensure React is imported
+import ReactDOM from 'react-dom';
 import App from "./App";
 
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
