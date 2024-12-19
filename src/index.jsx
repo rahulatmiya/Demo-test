c8b64b8ca24d1ff80c160b7865419d9671d30f2a
rahulatmiya <rahulatmiya@gmail.com>
2024-12-20 01:02:50 +0530

    Componets created

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,14 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+ import { createRoot } from "react-dom/client";
 import React from 'react'; // Ensure React is imported
 import ReactDOM from 'react-dom';
 import App from "./App";
-import Date from "./Date"
-
-
-
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
-root.render(
+ root.render(
   <React.StrictMode>
-    <App />
-    <Date />
+    <App />   
   </React.StrictMode>
-);
+); 
